0b6935a007e9be7d756f9b6c06a63f0c22f78bfc
DelBogdan <delbogdan@yandex.ru>
2026-03-06 12:03:07 +0300

    Настройки автотестов с Pydantic. Практика работы с настройками

--- tests/authentication/test_registration.py
+++ tests/authentication/test_registration.py
@@ -9,6 +9,8 @@ from allure_commons.types import Severity
 
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.routes import AppRoute
+from config import settings
 
 
 @pytest.mark.regression
@@ -24,16 +26,16 @@ class TestRegistration:
     @allure.title("Registration with correct email, username and password")
     @allure.severity(Severity.CRITICAL)
     def test_successful_registration(self, registration_page: RegistrationPage, dashboard_page: DashboardPage):
-        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.visit(AppRoute.REGISTRATION)
         registration_page.registration_form.check_visible(
             email="",
             username="",
             password=""
         )
         registration_page.registration_form.fill(
-            email="user.name@gmail.com",
-            username="username",
-            password="password"
+            email=settings.test_user.email,
+            username=settings.test_user.username,
+            password=settings.test_user.password
         )
         registration_page.click_registration_button()
         dashboard_page.dashboard_toolbar_view.check_visible()
